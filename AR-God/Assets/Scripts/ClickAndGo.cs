8ca1444e89fd8e678383d0fb2ccbb960f3224032
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2021-02-03 23:08:43 +0100

    Player + navemesh + collider sur l'ile

---
+++ AR-God/Assets/Scripts/ClickAndGo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickAndGo : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private Transform _moveTarget;
+    public Transform cube;
+    private float timer;
+    private void Start()
+    {
+        agent = GetComponent(typeof(NavMeshAgent)) as NavMeshAgent;
+        timer = wanderTimer;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
