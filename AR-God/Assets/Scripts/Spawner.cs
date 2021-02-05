dd4ecf9527fca8e5501c36d3acaad6be36fe791c
Guillaume <guillaume.trem@outlook.fr>
2021-02-05 21:22:23 +0100

    ta mere island.zip 4 essai

---
+++ AR-God/Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Foodprefab;
+
+    public Vector3 center;
+    public Vector3 size;
+    public int nbMaxFood = 2;
+    int nbCurrentFood = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnFood()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x/2 / 2, size.x / 2), Random.Range(-size.y / 2 / 2, size.y / 2), Random.Range(-size.z / 2 / 2, size.z / 2));
+
+        Instantiate(Foodprefab, pos, Quaternion.identity);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+
+    public void SPAWN_BUTTON()
+    {
+        if (nbCurrentFood < nbMaxFood)
+        {
+            SpawnFood();
+            ++nbCurrentFood;
+        }
+    }
+}
