11ce388bda51c3f71f13954f87b985683e8afaf1
Guillaume <guillaume.trem@outlook.fr>
2021-02-06 20:49:22 +0100

    le bateau arrete au quai

--- AR-God/Assets/Island/Prefabs/Scripts/BateauMove.cs
+++ AR-God/Assets/Island/Prefabs/Scripts/BateauMove.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class BateauMove : MonoBehaviour
 {
     public Transform target;
-    public GameObject bateau;
     public Transform tmp;
-
-    [Range(3f, 10f)]
+    public Transform Quai;
+    bool found = false;
+    [Range(5f, 20f)]
     public float runSpeed;
+    float step;
+    public int timer = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +21,34 @@ public class BateauMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         tmp.LookAt(target);
         tmp.Rotate(0, -90, 0);
         tmp.RotateAround(target.position, target.transform.up, runSpeed * Time.deltaTime);
         tmp.eulerAngles = new Vector3(0, tmp.eulerAngles.y, 0);
+
+        if (Vector3.Distance(tmp.position, Quai.position) < 0.1)
+        {
+            BateauStop();
+        }
+        
+    }
+
+    void BateauStop()
+    {
+        transform.position = Quai.position;
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        while (timer > 0)
+        {
+
+            yield return new WaitForSeconds(4);
+            timer--;
+        }
+        timer = 5;
+        found = false;
     }
 }
