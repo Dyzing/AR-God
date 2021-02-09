83717198a57e0ec0330cf16a0e38a13ed1740a44
Guillaume <guillaume.trem@outlook.fr>
2021-02-09 16:13:35 +0100

    projet organise avec les sons. Fusion des derniers pushs

---
+++ AR-God/Assets/Scripts/VolcanExplosion.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanExplosion : MonoBehaviour
+{
+    public Transform cratere;
+    public GameObject explosionVolcanParticles;
+    public int timer = 3;
+    private bool doitExploser = false;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doitExploser)
+        {
+            VolcanExplosionWithCoroutine();
+        }
+    }
+
+
+    void VolcanExplosionWithCoroutine()
+    {
+        Instantiate(explosionVolcanParticles, cratere.position, Quaternion.identity);
+        source.Play();
+        doitExploser = false;
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(5);
+            timer--;
+        }
+        timer = 3;
+        doitExploser = true;
+    }
+}
---
+++ AR-God/Assets/Scripts/csDestroyEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csDestroyEffect : MonoBehaviour {
+    float time = 0.0f;
+    void Update ()
+    {
+        time += Time.deltaTime;
+	    if(time > 5)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
